3f2b6ef18e25e42d8008eb427256db14d93e53e8
barry <friendship-119@163.com>
2023-11-15 18:10:13 +0800

    update

---
+++ README.md
@@ -0,0 +1,32 @@
+# 计算 hbase 表的一些 统计信息
+# QuickStart
+##  注册
+修改emr集群， 添加如下配置：
+```shell
+<property>
+   <name>hbase.coprocessor.user.region.classes</name>
+   <value>org.apache.hadoop.hbase.coprocessor.AggregateImplementation</value>
+</property>
+```
+打开hbase shell
+```shell
+sudo hbase shell
+```
+
+```给表注册coproccessor
+disable 'your-table-name'
+alter 'your-table-name', METHOD => 'table_att', 'coprocessor' => '|org.apache.hadoop.hbase.coprocessor.AggregateImplementation|1001|'
+enable 'your-table-name'
+```
+
+
+## 运行代码
+编译打包后，把jar包 （带有with-dependencies.jar）放到emr主节点上
+
+运行
+```shell
+java -jar stathbase-0.0.1-jar-with-dependencies.jar
+```
+
+## 注意
+1. 代码编译环境最好和emr master环境一致，或者代码提交到emr master上执行
---
+++ src/main/java/com/nwcd/sungrow/app.java
@@ -0,0 +1,44 @@
+package com.nwcd.sungrow;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+
+import org.apache.hadoop.hbase.client.Scan;
+
+import org.apache.hadoop.hbase.client.coprocessor.AggregationClient;
+import org.apache.hadoop.hbase.client.coprocessor.LongColumnInterpreter;
+import org.apache.hadoop.hbase.TableName;
+
+
+public class app {
+
+    public static void main(String[] args) {
+        try {
+            String strTb = args[1];
+            // 配置
+            Configuration conf = HBaseConfiguration.create();
+
+            // 创建 AggregationClient
+            AggregationClient aggregationClient = new AggregationClient(conf);
+
+            // HBase 表名
+            TableName tableName = TableName.valueOf(strTb);
+
+            // 创建 Scan 对象
+            Scan scan = new Scan();
+
+            // 列解释器，用于指定聚合操作的列类型
+            LongColumnInterpreter columnInterpreter = new LongColumnInterpreter();
+
+            // 执行 rowCount 聚合操作
+            long rowCount = aggregationClient.rowCount(tableName, columnInterpreter, scan);
+
+            // 打印结果
+            System.out.println("Total row count: " + rowCount);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
